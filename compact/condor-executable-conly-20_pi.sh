8bd355a3b9f6e6ee4fa52c05c80c510e7d924e60
Sarah <sarah.eno@cern.ch>
2023-09-19 09:38:45 -0400

    typo

--- compact/condor-executable-conly-20_pi.sh
+++ compact/condor-executable-conly-20_pi.sh
@@ -17,7 +17,7 @@ echo "ran thisdd4hep"
 #
 
 
- ddsim --compactFile=/home/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/DRConly.xml --runType=batch -G --steeringFile /home/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/SCEPCALsteering.py --outputFile=./output/out_conly_20GeV_pi-_100.root --part.userParticleHandler='' -G --gun.position="0.,0.,-402*cm." --gun.direction "0 0 1" --gun.energy "20*GeV" --gun.particle="pi-" -N 1000 >& ./output/sce_pi_conly_20.log
+ ddsim --compactFile=/home/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/DRConly.xml --runType=batch -G --steeringFile /home/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/SCEPCALsteering.py --outputFile=./output/out_conly_20GeV_pi-_100.root --part.userParticleHandler='' -G --gun.position="0.,0.,-402*cm" --gun.direction "0 0 1" --gun.energy "20*GeV" --gun.particle="pi-" -N 1000 >& ./output/sce_pi_conly_20.log
 
 
 
