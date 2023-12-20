7c02ffc70837cfc4e4c04a5c3d10a264b3a6a0ce
Sarah <sarah.eno@cern.ch>
2023-12-20 17:37:13 -0500

    old

--- compact/jobs/condor-executable-fSCEPonly-30_GeV-e.sh
+++ compact/jobs/condor-executable-fSCEPonly-30_GeV-e.sh
@@ -7,7 +7,7 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_102b/x86_64-centos7-gcc11-opt/setup.sh
 echo "ran setup"
 source  /data/users/eno/CalVision/dd4hep/DD4hep/bin/thisdd4hep.sh
 echo "ran thisdd4hep"
-ddsim --compactFile=/home/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/DRFSCEPonly.xml --runType=batch -G --steeringFile /home/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/SCEPCALsteering.py --outputFile=/data/users/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/output/out_fSCEPonly_20GeV_e-_100.root --part.userParticleHandler= -G --gun.position="0.,0.,-210*cm" --gun.direction "0 0.05 0.99875" --gun.energy "30*GeV" --gun.particle="e-" -N 100 >& /data/users/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/output/sce_e_fSCEPonly_30.log
+ddsim --compactFile=/home/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/DRFSCEPonly.xml --runType=batch -G --steeringFile /home/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/SCEPCALsteering.py --outputFile=/data/users/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/output/out_fSCEPonly_30GeV_e-_100.root --part.userParticleHandler= -G --gun.position="0.,0.,-210*cm" --gun.direction "0 0.05 0.99875" --gun.energy "30*GeV" --gun.particle="e-" -N 100 >& /data/users/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/output/sce_e_fSCEPonly_30.log
 exitcode=$?
 echo ""
 END_TIME=`/bin/date`
--- compact/jobs/condor-executable-fSCEPonly-30_GeV-pi.sh
+++ compact/jobs/condor-executable-fSCEPonly-30_GeV-pi.sh
@@ -7,7 +7,7 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_102b/x86_64-centos7-gcc11-opt/setup.sh
 echo "ran setup"
 source  /data/users/eno/CalVision/dd4hep/DD4hep/bin/thisdd4hep.sh
 echo "ran thisdd4hep"
-ddsim --compactFile=/home/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/DRFSCEPonly.xml --runType=batch -G --steeringFile /home/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/SCEPCALsteering.py --outputFile=/data/users/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/output/out_fSCEPonly_20GeV_pi-_100.root --part.userParticleHandler= -G --gun.position="0.,0.,-210*cm" --gun.direction "0 0.05 0.99875" --gun.energy "30*GeV" --gun.particle="pi-" -N 100 >& /data/users/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/output/sce_pi_fSCEPonly_30.log
+ddsim --compactFile=/home/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/DRFSCEPonly.xml --runType=batch -G --steeringFile /home/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/SCEPCALsteering.py --outputFile=/data/users/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/output/out_fSCEPonly_30GeV_pi-_100.root --part.userParticleHandler= -G --gun.position="0.,0.,-210*cm" --gun.direction "0 0.05 0.99875" --gun.energy "30*GeV" --gun.particle="pi-" -N 100 >& /data/users/eno/CalVision/dd4hep/DD4hep/examples/DualTestBeam/compact/output/sce_pi_fSCEPonly_30.log
 exitcode=$?
 echo ""
 END_TIME=`/bin/date`
