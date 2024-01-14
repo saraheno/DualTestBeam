e05c530916c761089f4ad4eb39286b5a8f3ded74
Sarah <sarah.eno@cern.ch>
2024-01-14 17:51:16 -0500

    remove things re Markus

--- src/DRFtubeFiber_geo.cpp
+++ src/DRFtubeFiber_geo.cpp
@@ -255,49 +255,26 @@ static Ref_t create_detector(Detector& description, xml_h e, SensitiveDetector s
 
   //  scint
   PlacedVolume abs1_phv = tower1Vol.placeVolume( abs1Vol, tra);
-  abs1_phv.addPhysVolID("fiber",0);
-  abs1_phv.addPhysVolID("abs",1);
-  abs1_phv.addPhysVolID("phdet",0);
-  abs1_det.setPlacement(abs1_phv);
+  abs1_phv.addPhysVolID("type",0);
   PlacedVolume absh1_phv = abs1Vol.placeVolume( absh1Vol, tra2);
-  absh1_phv.addPhysVolID("fiber",0);
-  absh1_phv.addPhysVolID("abs",2);
-  absh1_phv.addPhysVolID("phdet",0);
-  absh1_det.setPlacement(absh1_phv);
+  absh1_phv.addPhysVolID("type",5);
   PlacedVolume fiber1_phv = absh1Vol.placeVolume( fiber1Vol, tra2);
-  fiber1_phv.addPhysVolID("fiber",1);
-  fiber1_phv.addPhysVolID("abs",0);
-  fiber1_phv.addPhysVolID("phdet",0);
-  fiber1_det.setPlacement(fiber1_phv);
+  fiber1_phv.addPhysVolID("type",2);
   PlacedVolume photod1_phv = tower1Vol.placeVolume( photod1Vol, tra4);
-  photod1_phv.addPhysVolID("fiber",0);
-  photod1_phv.addPhysVolID("abs",0);
-  photod1_phv.addPhysVolID("phdet",1);
-  photod1_det.setPlacement(photod1_phv);
+  photod1_phv.addPhysVolID("type",4);
+
 
 
 
   //  quartz
   PlacedVolume abs2_phv = tower2Vol.placeVolume( abs2Vol, tra);
-  abs2_phv.addPhysVolID("fiber",0);
-  abs2_phv.addPhysVolID("abs",1);
-  abs2_phv.addPhysVolID("phdet",0);
-  abs2_det.setPlacement(abs2_phv);
+  abs2_phv.addPhysVolID("type",0);
   PlacedVolume absh2_phv = abs2Vol.placeVolume( absh2Vol, tra2);
-  absh2_phv.addPhysVolID("fiber",0);
-  absh2_phv.addPhysVolID("abs",2);
-  absh2_phv.addPhysVolID("phdet",0);
-  absh2_det.setPlacement(absh2_phv);
+  absh2_phv.addPhysVolID("type",5);
   PlacedVolume fiber2_phv = absh2Vol.placeVolume( fiber2Vol, tra2);
-  fiber2_phv.addPhysVolID("fiber",2);
-  fiber2_phv.addPhysVolID("abs",0);
-  fiber2_phv.addPhysVolID("phdet",0);
-  fiber2_det.setPlacement(fiber2_phv);
+  fiber2_phv.addPhysVolID("type",1);
   PlacedVolume photod2_phv = tower1Vol.placeVolume( photod2Vol, tra4);
-  photod2_phv.addPhysVolID("fiber",0);
-  photod2_phv.addPhysVolID("abs",0);
-  photod2_phv.addPhysVolID("phdet",2);
-  photod2_det.setPlacement(photod2_phv);
+  photod2_phv.addPhysVolID("fiber",3);
 
 
   // clone towers and place in row
@@ -365,6 +342,6 @@ static Ref_t create_detector(Detector& description, xml_h e, SensitiveDetector s
   return sdet;
 }
 
-DECLARE_DETELEMENT(DD4hep_DRFtubeFiber,create_detector)
+DECLARE_DETELEMENT(DRFtubeFiber,create_detector)
 
-DECLARE_DEPRECATED_DETELEMENT(DRFtubeFiber,create_detector)
+
