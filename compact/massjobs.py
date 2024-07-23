a5b5878d38aa203964978ab74a97eba8923282e3
snabili <snabili@umd.edu>
2024-07-23 08:39:26 -0400

    small changes

--- compact/massjobs.py
+++ compact/massjobs.py
@@ -19,18 +19,16 @@ print("args.name=%s" % args.geometry)
 parent_dir = os.getcwd()
 source_dir = os.getcwd()+'/../../..'
 
-lis = os.listdir()
-if len([i for i in lis if i == 'output'])==0: #check if the output and jobs directory are made before
-  os.mkdir('output')
-  os.mkdir('jobs')
-outputarea = os.getcwd() + '/output/'
-hostarea = os.getcwd() + '/jobs/'
+if not os.path.exists('output'):
+    os.makedirs('output/' + args.geometry)
+    os.makedirs('jobs/' + args.geometry)
+outputarea = os.getcwd() + '/output/' + args.geometry + '/'
+hostarea = os.getcwd() + '/jobs/' + args.geometry + '/'
 
-nenergy=3
 energies=[10,15,20,25,30,35,40,45,50,100]
 name="condor-executable-"+args.geometry+"-dial-"
 
-# create the .sh files for electrons
+# create the .sh files
 shfile = open(hostarea+name+args.particle+'.sh',"w")
 shfile.write('#!/bin/bash'+'\n')
 shfile.write('cd '+ parent_dir + '\n')
@@ -43,31 +41,30 @@ shfile.write('echo "machine is centos${VERSION_ID%.*}"' + '\n')
 shfile.write('source /cvmfs/sft.cern.ch/lcg/views/LCG_102b/x86_64-centos${VERSION_ID%.*}-gcc11-opt/setup.sh' + '\n')
 shfile.write('source '+source_dir+'/install/bin/thisdd4hep.sh'+'\n')
 shfile.write('echo "ran setup"'+'\n')
-
 for i in energies[:3]:
-  shfile.write('ddsim --compactFile='+parent_dir+'/DR'+args.geometry+'.xml --runType=batch -G --steeringFile '+parent_dir+'/SCEPCALsteering.py --outputFile='+outputarea+'out_'+args.geometry+'-dial_'+args.particle+str(i)+'_.root --part.userParticleHandler='' -G --gun.position="0.,0*mm,-1*cm" --gun.direction "0 0.0 1." --gun.energy "'+str(i)+'*GeV" --gun.particle="'+args.particle+'" -N 50 >& '+outputarea+'sce_e_'+args.geometry+'-dial_'+str(i)+'.log'+'\n')
+  shfile.write('ddsim --compactFile='+parent_dir+'/DR'+args.geometry+'.xml --runType=batch -G --steeringFile '+parent_dir+'/SCEPCALsteering.py --outputFile='+outputarea+'out_'+args.geometry+'-dial_'+args.particle+str(i)+'_.root --part.userParticleHandler='' -G --gun.position="0.,0*mm,-1*cm" --gun.direction "0 0.0 1." --gun.energy "'+str(i)+'*GeV" --gun.particle="'+args.particle+'" -N 100 >& '+outputarea+'Log_'+args.geometry+'-dial_'+str(i)+'_'+args.particle+'.log'+'\n')
 shfile.write('exitcode=$?'+'\n')
 shfile.write('echo ""'+'\n')
 shfile.write('END_TIME=`/bin/date`'+'\n')
 shfile.write('echo "finished at $END_TIME"'+'\n')
 shfile.write('exit $exitcode'+'\n')
 shfile.close()
-print("file closed")
+print("sh file closed")
 
+print(hostarea+name+args.particle)
 
-# create the .jdl files for electrons
+# create the .jdl files
 jdlfile = open(hostarea+name+args.particle+'.jdl',"w")
 jdlfile.write("universe = vanilla"+'\n')
 jdlfile.write("Executable ="+hostarea+name+args.particle+".sh"+'\n')
 jdlfile.write("should_transfer_files = NO"+'\n')
 jdlfile.write("Requirements = TARGET.FileSystemDomain == \"privnet\""+'\n')
-jdlfile.write("Output = "+hostarea+name+args.particle+"$(cluster)_$(process).stdout"+'\n')
-jdlfile.write("Error = "+hostarea+name+args.particle+"$(cluster)_$(process).stderr"+'\n')
+jdlfile.write("Output = "+hostarea+args.particle+"$(cluster)_$(process).stdout"+'\n')
+jdlfile.write("Error = "+hostarea+args.particle+"$(cluster)_$(process).stderr"+'\n')
 jdlfile.write("Log = "+hostarea+name+args.particle+"$(cluster)_$(process).condor"+'\n')
 jdlfile.write("Arguments = $(process)"+'\n')
-jdlfile.write('Queue '+str(nenergy) + '\n')
-#for i in energies[:3]: jdlfile.write(str(i)+ ' ')
-#print("file closed")
+jdlfile.write('Queue '+str(len(energies[:3])) + '\n')
+print("jdl file closed")
 jdlfile.close()
 
 
@@ -77,6 +74,3 @@ f.write('chmod +x '+hostarea+'*'+'\n')
 f.write("condor_submit "+hostarea+name+args.particle+'.jdl'+'\n')
 f.write("condor_q"+'\n')
 f.close()
-
-
-
